af44cab0f5a047c9d0bd505d69e7b426eb0f88dc
iamharshtambe <harshtambe.dev@gmail.com>
2025-06-14 14:17:00 +0530

    reinitialized the project

--- src/server.js
+++ src/server.js
@@ -3,7 +3,15 @@ import express from 'express';
 const app = express();
 const port = 5000;
 
-app.get('/', (req, res) => {
+app.get('/request', (req, res) => {
+   res.send('This is GET request!');
+});
+
+app.post('/request', (req, res) => {
+   res.send('This is POST request!');
+});
+
+app.use('/test', (req, res) => {
    res.send('Hello, World!');
 });
 
