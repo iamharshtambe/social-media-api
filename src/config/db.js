537695ca9a2d1e764ddd5039033ca12f56d727a7
iamharshtambe <harshtambe.dev@gmail.com>
2025-06-15 13:38:00 +0530

    connected database

---
+++ src/config/db.js
@@ -0,0 +1,7 @@
+import mongoose from 'mongoose';
+
+export async function connectDB() {
+   await mongoose.connect(
+      'mongodb+srv://iamharshtambe:helloworld@eternal.fk10f.mongodb.net/'
+   );
+}
--- src/server.js
+++ src/server.js
@@ -1,19 +1,16 @@
 import express from 'express';
-import { adminAuth } from './middlewares/auth.js';
+import { connectDB } from './config/db.js';
 
 const app = express();
 const port = 5000;
 
-app.use('/admin', adminAuth);
-
-app.get('/admin/addData', (req, res) => {
-   res.send('Data added successfully...');
-});
-
-app.get('/admin/deleteData', (req, res) => {
-   res.send('Data deleted successfully...');
-});
-
-app.listen(port, () => {
-   console.log(`Server is running on http://localhost:${port}`);
-});
+connectDB()
+   .then(() => {
+      console.log('Database connected successfully');
+      app.listen(port, () => {
+         console.log(`Server is running on http://localhost:${port}`);
+      });
+   })
+   .catch((err) => {
+      console.error('Database connection failed');
+   });
