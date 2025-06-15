2e039a0ce5eb07b24fb27414fd981257963633c9
iamharshtambe <harshtambe.dev@gmail.com>
2025-06-15 18:59:24 +0530

    added dummy data to database

--- src/server.js
+++ src/server.js
@@ -1,8 +1,24 @@
 import express from 'express';
 import { connectDB } from './config/db.js';
+import { User } from './models/user.js';
 
 const app = express();
 const port = 5000;
+
+app.post('/signup', async (req, res) => {
+   const user = new User({
+      firstName: 'Harsh',
+      lastName: 'Tambe',
+      email: 'harshtambe19@gmaul.com',
+      password: '241331',
+      age: 21,
+      gender: 'Male',
+   });
+
+   await user.save();
+
+   res.send('User added successfully');
+});
 
 connectDB()
    .then(() => {
