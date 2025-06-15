22da4e60023fbfdb0fdbe58162d71d824a72d45e
iamharshtambe <harshtambe.dev@gmail.com>
2025-06-15 18:17:55 +0530

    added db to url

--- src/config/db.js
+++ src/config/db.js
@@ -2,6 +2,6 @@ import mongoose from 'mongoose';
 
 export async function connectDB() {
    await mongoose.connect(
-      'mongodb+srv://iamharshtambe:helloworld@eternal.fk10f.mongodb.net/'
+      'mongodb+srv://iamharshtambe:helloworld@eternal.fk10f.mongodb.net/social-media-db'
    );
 }
