42f67ca07eb5f4953f54bdf264d5fee88f3ab78e
iamharshtambe <harshtambe.dev@gmail.com>
2025-06-18 21:39:32 +0530

    added /profile/edit

--- src/routes/profileRouter.js
+++ src/routes/profileRouter.js
@@ -1,13 +1,36 @@
 import express from 'express';
 import { userAuth } from '../middlewares/auth.js';
+import { validateEditProfileData } from '../utils/validation.js';
 
 export const profileRouter = express.Router();
 
-profileRouter.get('/profile', userAuth, async (req, res) => {
+profileRouter.get('/profile/view', userAuth, async (req, res) => {
    try {
       const user = req.user;
 
       res.send(user);
+   } catch (err) {
+      res.status(404).send(`Error: ${err.message}`);
+   }
+});
+
+profileRouter.patch('/profile/edit', userAuth, async (req, res) => {
+   try {
+      if (!validateEditProfileData) {
+         throw new Error('Invalid edit request');
+      }
+
+      const loggedInUser = req.user;
+
+      Object.keys(req.body).forEach(
+         (key) => (loggedInUser[key] = req.body[key])
+      );
+
+      await loggedInUser.save();
+
+      res.json({
+         message: `${loggedInUser.firstName} your profile updated successfully`,
+      });
    } catch (err) {
       res.status(404).send(`Error: ${err.message}`);
    }
