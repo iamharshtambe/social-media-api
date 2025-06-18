79f11a96ea24616d60571836fc8df6416eee6e21
iamharshtambe <harshtambe.dev@gmail.com>
2025-06-18 18:15:06 +0530

    added auth router

---
+++ src/routes/authRouter.js
@@ -0,0 +1,57 @@
+import express from 'express';
+import { validateSignupData } from '../utils/validation.js';
+import bcrypt from 'bcrypt';
+import { User } from '../models/user.js';
+
+export const authRouter = express.Router();
+
+authRouter.post('/signup', async (req, res) => {
+   try {
+      validateSignupData(req);
+
+      const { firstName, lastName, email, password } = req.body;
+
+      const passwordHash = await bcrypt.hash(password, 10);
+
+      const user = new User({
+         firstName,
+         lastName,
+         email,
+         password: passwordHash,
+      });
+
+      await user.save();
+
+      res.send('User added successfully');
+   } catch (err) {
+      res.status(404).send(`Error: ${err.message}`);
+   }
+});
+
+authRouter.post('/login', async (req, res) => {
+   try {
+      const { email, password } = req.body;
+
+      const user = await User.findOne({ email: email });
+
+      if (!user) {
+         throw new Error('Invalid credentials!');
+      }
+
+      const isPasswordValid = await user.validatePassword(password);
+
+      if (isPasswordValid) {
+         const token = await user.getJWT();
+
+         res.cookie('token', token, {
+            expires: new Date(Date.now() + 24 * 3600000),
+         });
+
+         res.send('Login Successfully');
+      } else {
+         throw new Error('Invalid credentails!');
+      }
+   } catch (err) {
+      res.status(404).send(`Error: ${err.message}`);
+   }
+});
