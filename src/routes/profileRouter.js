ce5d1c57cfcb0638084bbfa874f17cbae4043a57
iamharshtambe <harshtambe.dev@gmail.com>
2025-06-18 23:24:36 +0530

    added /profile/edit/password

--- src/routes/profileRouter.js
+++ src/routes/profileRouter.js
@@ -1,6 +1,10 @@
 import express from 'express';
 import { userAuth } from '../middlewares/auth.js';
-import { validateEditProfileData } from '../utils/validation.js';
+import {
+   validateEditProfileData,
+   validatePassword,
+} from '../utils/validation.js';
+import bcrypt from 'bcrypt';
 
 export const profileRouter = express.Router();
 
@@ -16,7 +20,7 @@ profileRouter.get('/profile/view', userAuth, async (req, res) => {
 
 profileRouter.patch('/profile/edit', userAuth, async (req, res) => {
    try {
-      if (!validateEditProfileData) {
+      if (!validateEditProfileData(req)) {
          throw new Error('Invalid edit request');
       }
 
@@ -29,7 +33,29 @@ profileRouter.patch('/profile/edit', userAuth, async (req, res) => {
       await loggedInUser.save();
 
       res.json({
-         message: `${loggedInUser.firstName} your profile updated successfully`,
+         message: `${loggedInUser.firstName}, your profile updated successfully`,
+      });
+   } catch (err) {
+      res.status(404).send(`Error: ${err.message}`);
+   }
+});
+
+profileRouter.patch('/profile/edit/password', userAuth, async (req, res) => {
+   try {
+      if (!validatePassword(req)) {
+         throw new Error('Enter a strong password');
+      }
+
+      const passwordHash = await bcrypt.hash(req.body.password, 10);
+
+      const loggedInUser = req.user;
+
+      loggedInUser.password = passwordHash;
+
+      await loggedInUser.save();
+
+      res.json({
+         message: `${loggedInUser.firstName}, your password updated successfully`,
       });
    } catch (err) {
       res.status(404).send(`Error: ${err.message}`);
