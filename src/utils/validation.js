775fa806eb8ecf3c117919c4d305f463439da12b
iamharshtambe <harshtambe.dev@gmail.com>
2025-06-18 21:39:00 +0530

    added validation to edit profile

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -11,3 +11,13 @@ export function validateSignupData(req) {
       throw new Error('Enter a strong password');
    }
 }
+
+export function validateEditProfileData(req) {
+   const allowedEditFields = [firstName, lastName, age, gender, about, skills];
+
+   const isAllowed = Object.keys(req.body).every((field) =>
+      allowedEditFields.includes(field)
+   );
+
+   return isAllowed;
+}
