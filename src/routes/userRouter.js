f9f1d31aa9b443bbae9c5da3fc52e1dbb90fc3c4
iamharshtambe <harshtambe.dev@gmail.com>
2025-06-20 21:21:52 +0530

    added api to get all the connections of users

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -22,4 +22,24 @@ userRouter.get('/user/request/received', userAuth, async (req, res) => {
    }
 });
 
-userRouter.get('/user/request/connections', userAuth, (req, res) => {});
+userRouter.get('/user/request/connections', userAuth, async (req, res) => {
+   try {
+      const loggedInUser = req.user;
+
+      const connectionRequest = await ConnectionRequest.find({
+         $or: [
+            { toUserId: loggedInUser._id, status: 'accepted' },
+            { fromUserId: loggedInUser._id, status: 'accepted' },
+         ],
+      }).populate('fromUserId', ['firstName', 'lastName']);
+
+      const data = connectionRequest.map((row) => row.fromUserId);
+
+      res.json({
+         message: 'You have following connections:',
+         data,
+      });
+   } catch (err) {
+      res.status(404).send(`Error: ${err.message}`);
+   }
+});
